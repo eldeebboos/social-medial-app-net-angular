9287195d9caa6a846719669a0267ce71bddc2899
eldeebboos <eldeeb_boos@yahoo.com>
2024-11-19 20:27:43 +0200

    Initial of project webapi

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = context.Users.ToListAsync();
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = context.Users.FindAsync(id);
+
+        if (user == null)
+            return NotFound();
+        return Ok(user);
+    }
+
+}
